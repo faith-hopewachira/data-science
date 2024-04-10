229dbe759e709f3077638ff43bab8e34ab9b9659
FaithHope <wachiraafaith>
2024-04-10 21:53:36 +0300

    add stacks

--- stack.py
+++ stack.py
@@ -1,54 +1,55 @@
-stack = []
+# stack = []
+stack1 = []
 #ADDING THE NUMBERS TO THE STACK
-# stack.append(10)
-# stack.append(20)
-# stack.append(30)
-# stack.append(40)
-# print(stack)
+stack1.append(10)
+stack1.append(20)
+stack1.append(30)
+stack1.append(40)
+print(stack1)
 
-# # #REMOVING NUMBERS FROM THE STACK
-# # stack.pop()
-# # stack.pop()
-# # stack.pop()
-# # stack.pop()
-# # print(stack)
+# # # #REMOVING NUMBERS FROM THE stack1
+# stack1.pop()
+# stack1.pop()
+# stack1.pop()
+# stack1.pop()
+# print(stack1)
 
-# #ACCESSING THE LAST ELEMENTIN THE STACK
-# print(stack[-1])
-# print(stack[0])
-# print(stack[1])
-# print(stack[2])
-# print(stack[3])
+# #ACCESSING THE LAST ELEMENTIN THE stack1
+print(stack1[-1])
+print(stack1[0])
+print(stack1[1])
+print(stack1[2])
+print(stack1[3])
 
 
 # PUSH FUNCTION
-def push():
-    element = input("Enter the element:")
-    stack.append(element)
-    print(element)
+# def push():
+#     element = input("Enter the element:")
+#     stack.append(element)
+#     print(element)
 
-# POP FUNCTION
-def pop_element():
-    #IF STACK IS EMPTY
-    if not stack:
-        print("stack is empty!")
-    else:
-        e = stack.pop()
-        print("removed element:",e)
-        print(stack)
+# # POP FUNCTION
+# def pop_element():
+#     #IF STACK IS EMPTY , DO NOT POP
+#     if not stack:
+#         print("stack is empty!")
+#     else:
+#         e = stack.pop()
+#         print("removed element:",e)
+#         print(stack)
 
-# USER TO SELECT THE OPERATION
-while True:
-    print("Select the option 1.push 2.pop 3.quit")
-    choice = int(input())
-    if choice ==1:
-        push()
-    elif choice ==2:
-        pop_element()
-    elif choice ==3:
-        break
-    else:
-        print("Enter the correct operation!")
+# # USER TO SELECT THE OPERATION
+# while True:
+#     print("Select the option 1.push 2.pop 3.quit")
+#     choice = int(input())
+#     if choice ==1:
+#         push()
+#     elif choice ==2:
+#         pop_element()
+#     elif choice ==3:
+#         break
+#     else:
+#         print("Enter the correct operation!")
 
 
 
